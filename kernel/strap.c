7383d989508892aa747dfe200e75a2f25ddd8756
Crystalclear <704542753@qq.com>
2025-12-08 09:15:03 +0800

    my work on lab1_1 is done

--- kernel/strap.c
+++ kernel/strap.c
@@ -8,7 +8,6 @@
 #include "syscall.h"
 
 #include "spike_interface/spike_utils.h"
-
 //
 // handling the syscalls. will call do_syscall() defined in kernel/syscall.c
 //
@@ -22,6 +21,7 @@ static void handle_syscall(trapframe *tf) {
   // kernel/syscall.c) to conduct real operations of the kernel side for a syscall.
   // IMPORTANT: return value should be returned to user app, or else, you will encounter
   // problems in later experiments!
+  tf->regs.a0 = do_syscall(tf->regs.a0,tf->regs.a1,tf->regs.a2,tf->regs.s3,tf->regs.a4,tf->regs.a5,tf->regs.a6,tf->regs.a7);
   panic( "call do_syscall to accomplish the syscall and lab1_1 here.\n" );
 
 }
